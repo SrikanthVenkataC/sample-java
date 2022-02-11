c71fc22015b73363e336669b7d746d63b8be12c1
SrikanthVenkataC <81322035+SrikanthVenkataC@users.noreply.github.com>
2022-02-11 13:05:51 +0530

    Test (#8)
    * Updated HelloWorld.java new message
    
    * new stmt added
    
    * stmt 2 modified
    
    * welcome to git msg added
    
    * if stmt added
    
    * final stmt added
    
    Co-authored-by: “Srikanth <“cvssrikanth@gmail.com”>

--- Hello.java
+++ Hello.java
@@ -1,7 +1,14 @@
 class Hello{
 public static void main(String args[]){
+  var1=100
+  System.out.println(" Hello World .. welcome to git");
+  System.out.println(" Added new output stmt ... modify this");
+  if (var1 ==10){
+		  System.out.println(" var1 is equal to 100");
+	}else {		  System.out.println(" var1 is NOT equal to 100"); }
 
-  System.out.println(" Hello World ");
+   System.out.println(" End of the main);
+
 }
 
 }
