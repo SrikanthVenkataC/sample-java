a142bf62684f26bcda6d5b88418cd8d1d6d05fca
“Srikanth <cvssrikanth@gmail.com>
2022-02-11 12:53:00 +0530

    stmt 2 modified

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ class Hello{
 public static void main(String args[]){
 
   System.out.println(" Hello World ");
-  System.out.println(" Added new output stmt ");
-  System.out.println(" New SOP stmt added... ");
+  System.out.println(" Added new output stmt ... modify this");
+  
 
 }
 
