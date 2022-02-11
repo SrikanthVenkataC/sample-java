ab18feca69f320d7edffa1568248dc0a17c10be5
“Srikanth <cvssrikanth@gmail.com>
2022-02-11 12:57:27 +0530

    if stmt added

--- Hello.java
+++ Hello.java
@@ -1,9 +1,11 @@
 class Hello{
 public static void main(String args[]){
-
+  var1=100
   System.out.println(" Hello World .. welcome to git");
   System.out.println(" Added new output stmt ... modify this");
-  
+  if (var1 ==10){
+		  System.out.println(" var1 is equal to 100");
+	}else {		  System.out.println(" var1 is NOT equal to 100"); }
 
 }
 
