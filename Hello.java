bab35734e12d74e197c4bb1cd73d36feb3e23ce3
SrikanthVenkataC <81322035+SrikanthVenkataC@users.noreply.github.com>
2022-02-11 12:55:35 +0530

    Feature jira1001 srikanth (#4)
    * new stmt added
    
    * stmt 2 modified
    
    * welcome to git msg added
    
    Co-authored-by: “Srikanth <“cvssrikanth@gmail.com”>

--- Hello.java
+++ Hello.java
@@ -1,8 +1,10 @@
 class Hello{
 public static void main(String args[]){
 
-  System.out.println(" Hello World ");
-  System.out.println(" Added new output stmt ");
+  System.out.println(" Hello World .. welcome to git");
+  System.out.println(" Added new output stmt ... modify this");
+  
+
 }
 
 }
